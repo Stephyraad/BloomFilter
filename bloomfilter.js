495e51495e1de27c576e8545cd7a4eccd086af8f
Stephanie Raad <stephy.raad@gmail.com>
2015-12-18 13:32:34 -0800

    [refactor] BloomFilter dynamic according to parameters

--- bloomfilter.js
+++ bloomfilter.js
@@ -1,14 +1,18 @@
 // Function BloomFilter Constructor
-
-// Function Parameters max = vector size, k = number of hash functions, n ~ number of items you expect to insert in the bitArray, p = false positive rate
+// Function Parameters:
+//    -> n ~ number of items you expect to insert in the bitArray
+//    -> p = false positive rate
 var BloomFilter = function(n, p) {
 	this.p = p;
+  // max size of the Array
 	this.max=  Math.floor( -n * Math.log(p) / (Math.log(2)^2) );
+  // number of Hashing Functions
 	this.k= Math.floor( this.max/n * Math.log(2) );
-	this.bitArray= [];
+  //Array where to store the bits
+  this.bitArray= [];
+  // Array of returned indexes from Hashing Functions
 	this.indexesArray = [];
-  this.num = 0;
-	
+  
 	var defaults = {
 		max: 100,
 		k: 2
@@ -20,98 +24,56 @@ var BloomFilter = function(n, p) {
   for(var i =0; i< this.max; i++){
   	this.bitArray[i] = 0;
   }
-};
 
-BloomFilter.prototype.allHashIndex = function(str) {
-	var num = this.num;
-	var max = this.max;
-	var k = this.k;
-	var indexesArray = this.indexesArray;
-
-  
-  function getIndexHashing(str, max, num) {
-    var hash = num;
-
-    for (var i = 0; i < str.length; i++) {
-    hash = (hash<<5) + hash + str.charCodeAt(i);
+  this.getIndexHashing = function(value, num) {
+  var hash = num;
+  for (var i = 0; i < value.length; i++) {
+    hash = (hash<<5) + hash + value.charCodeAt(i);
     hash = hash & hash; // Convert to 32bit integer
     hash = Math.abs(hash);
-    }
-    return hash % max;
+  }
+  return hash % this.max;
   };
+};
 
-	for(var i=0; i< k; i++) {
-    var x = getIndexHashing(str, max, num);
-    indexesArray.push(x);
-    num += 5000;
-	}
-  this.indexesArray = indexesArray;
-  // console.log("G ", this.indexesArray);
-  return indexesArray; 
-}
 
 // Method to set bitArray at location i to true
 BloomFilter.prototype.add = function(value) {
-  console.log("GG ", this.indexesArray);
+  var num =0;
+
+  for(var i=0; i< this.k; i++) {
+    this.indexesArray[i] = this.getIndexHashing(value, num);
+    num+=5000;
+  }
+
 	for(var i = 0; i< this.indexesArray.length; i++) {
 		this.bitArray[this.indexesArray[i]] = 1;
 	}
 };
 
-// Method to check if value at index is 1
+//Method to check if value at index is 1
 BloomFilter.prototype.checkIndex = function(value) {
-	var indexesArray = this.indexesArray;
-  console.log("BOOO", this.indexesArray);
+  var num =0;
+  for(var i=0; i< this.k; i++) {
+    this.indexesArray[i] = this.getIndexHashing(value, num);
+    num+=5000;
+  }
 
-	for(var i = 0; i < indexesArray.length; i++) {
-		if(this.bitArray[indexesArray[i]] === 0) {
-			return false;
-		}
-	}
-	return true;
+ for(var i = 0; i < this.indexesArray.length; i++) {
+   if(this.bitArray[this.indexesArray[i]] === 0) {
+     return false;
+   }
+ }
+ return true;
 };
 
-var a = new BloomFilter(10000, 0.001);
-console.log(a);
-console.log(a.allHashIndex("hee"));
-console.log("add", a.add("hee"));
-console.log("CHECK ", a.checkIndex("cat"))
+// ---------------- Test -------------------
+var bloomTest = new BloomFilter(10000, 0.001);
 
-// Hash Functions
-// var getIndexHashing = function(str, max) {
-//   var hash = 0;
-//   for (var i = 0; i < str.length; i++) {
-//     hash = (hash<<5) + hash + str.charCodeAt(i);
-//     hash = hash & hash; // Convert to 32bit integer
-//     hash = Math.abs(hash);
-//   }
-//   return hash % max;
-// };
-
-// var getIndexHashingTwo = function(str, max) {
-//   var hash = 5500;
-//   for (var i = 0; i < str.length; i++) {
-//     hash = (hash<<5) + hash + str.charCodeAt(i);
-//     hash = hash & hash; // Convert to 32bit integer
-//     hash = Math.abs(hash);
-//   }
-//   return hash % max;
-// };
-
-// Method to find all indexes for the many hashing functions
-// BloomFilter.prototype.getIndex = function(value) {
-//  var hash1 = getIndexHashing(value, this.max);
-//  var hash2 = getIndexHashingTwo(value, this.max);
-
-//  this.indexesArray = [ hash1, hash2 ];
-//  return this.indexesArray;
-// };
-
- //----- TEST ----- //
-var bloomVector = new BloomFilter(10000, 0.001);
-// bloomVector.getIndex('Hello World');
-// bloomVector.add("Hello World");
-// var check = bloomVector.checkIndex("Hello World");
-// console.log(check + " Prob rate: " + bloomVector.p + "%");
-
+console.log("bloomTest ", bloomTest)
+bloomTest.add("Hello World");
+bloomTest.add("Hello Javascript");
+console.log("Check ", bloomTest.checkIndex('Hello World'));
+console.log("Check False ", bloomTest.checkIndex('Hello Cat'));
+console.log("Check Two ", bloomTest.checkIndex("Hello Javascript"))
 
